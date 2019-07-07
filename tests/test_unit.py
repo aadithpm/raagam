6e22657c2f1da086eca048190860c34a950a212c
aadithpm <aadithpm@gmail.com>
2019-07-06 22:53:16 -0400

    Added test for extracting artist ID

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,39 @@
 import unittest
+from artist import artists_handler_spotify
 
 
 class Unittest(unittest.TestCase):
 
     def test_canary(self):
         self.assertTrue(True)
+
+
+class ArtistsHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_data_simple = {
+            "artists": {
+                "items": {
+                    0: {
+                        "id": "12Chz98pHFMPJEknJQMWvI",
+                        "name": "Muse",
+                    },
+                    1: {
+                        "id": "123456",
+                        "name": "I Don't Exist",
+                    }
+                }
+            }
+        }
+        self.test_data_multiple = {}
+
+    def test_extract_artist_gets_artist_data(self):
+        test_artist = self.test_data_simple["artists"]["items"][0]
+        name_to_use = test_artist["name"]
+        id_to_check = test_artist["id"]
+        response_handler = artists_handler_spotify.ArtistsResponse(
+            self.test_data_simple["artists"]
+        )
+
+        self.assertEquals(
+            id_to_check, response_handler.extract_artist_id(name_to_use))
