f384774704f35c3a0f050371f5cab5367fd2b17c
aadithpm <aadithpm@gmail.com>
2019-07-07 20:36:30 -0400

    Not worrying about fuzzy matches for now

--- artist/artists_handler_spotify.py
+++ artist/artists_handler_spotify.py
@@ -6,7 +6,6 @@ class ArtistsResponse():
         self.data = response
 
     def extract_artist_id(self, name):
-        # TODO: Handle multiple artists with similar name
         artists = self.data["items"]
         for artist_number in artists:
             if artists[artist_number]["name"] == name:
