590ff0ebe5898ba85b19317b9cf4967c39faa821
aadithpm <aadithpm@gmail.com>
2019-08-03 16:34:39 -0500

    Cleaning up some code

--- artist/artists_handler_spotify.py
+++ artist/artists_handler_spotify.py
@@ -4,9 +4,9 @@ class ArtistsResponse():
 
     def __init__(self, response):
         self.data = response
+        self.artists = self.data["items"]
 
     def extract_artist_id(self, name):
-        artists = self.data["items"]
-        for artist_number in artists:
-            if artists[artist_number]["name"] == name:
-                return artists[artist_number]["id"]
+        for artist_number in self.artists:
+            if self.artists[artist_number]["name"] == name:
+                return self.artists[artist_number]["id"]
